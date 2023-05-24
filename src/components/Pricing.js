5ea62e015590b4f25d632fa73e37ecabd66611ed
Joe-x007 <101728021+Joe-x007@users.noreply.github.com>
2023-05-24 22:49:40 +0200

    Add files via upload
    created home page

---
+++ src/components/Pricing.js
@@ -0,0 +1,14 @@
+// Hero.js
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <h1 className="hero-title">CyberPulse Creations</h1>
+            <p className="hero-slogan">Creating Vibrant Digital Experiences, One Pulse at a Time</p>
+        </div>
+    );
+}
+
+export default Hero;
